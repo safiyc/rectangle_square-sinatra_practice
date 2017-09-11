36bae2559197a91312d41697387f7e4f29fbfdca
Guest User <Guest@epicodus-191.local>
2017-09-10 17:42:20 -0700

    add working code for sinatra practice.

---
+++ app.rb
@@ -0,0 +1,22 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/rectangle')
+require('pry')
+
+get('/') do
+  erb(:input)
+end
+
+get('/output') do
+  @length = params.fetch('length')
+  @width = params.fetch('width')
+  rectangle = Rectangle.new(@length, @width)
+  binding.pry
+  if rectangle.square?
+    @string_to_display = "This is a square."
+  else
+    @string_to_display = "This is not a square."
+  end
+  erb(:output)
+end
